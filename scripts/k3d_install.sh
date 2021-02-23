ca226dfc27252de1bb7c358ec9f9b7ecabd9a841
J0hn-B <connect-wsman@hotmail.com>
2021-02-23 11:45:58 +0000

    initial commit

---
+++ scripts/k3d_install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Parameters
+CLUSTER=$(k3d cluster list multiserver)
+
+# Check if K3d exists
+if [ $(command -v k3d) ]; then
+    echo "--> k3d exists, skip install"
+else
+    echo "> > > k3d does not exist, installing now"
+    wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
+fi
+
+# Prepare the k3d cluster
+if [ "$CLUSTER" ]; then
+    echo "Cluster exists"
+else
+    echo "--> multiserver cluster does not exist, creating now"
+    k3d cluster create multiserver --servers 1 --agents 2
+fi
+
+# Install ArgoCD
+echo
+source ./argocd_install.sh
